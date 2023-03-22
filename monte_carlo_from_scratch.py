b503998d876b97a8b9e7d63193fc1e1a8215368c
Seb Bennett <seb@lumilinks.com>
2023-03-22 15:11:45 +0000

    Removed useless code

--- monte_carlo_from_scratch.py
+++ monte_carlo_from_scratch.py
@@ -24,28 +24,6 @@ class FinancialStock:
         print(f"Multiplier = {multiplier}")
         self.value *= multiplier
 
-
-
-def update_line(hl, new_data):
-    hl.set_xdata(np.append(hl.get_xdata(), new_data))
-    hl.set_ydata(np.append(hl.get_ydata(), new_data))
-    plt.draw()
         
 if __name__=='__main__':
     pass
-    # %matplotlib notebook
-    
-    
-    fig, ax = plt.subplots()
-    line, = ax.plot([]) 
-    hl, = plt.plot([], [])
-    
-    def animate(frame_num):
-        x = frame_num
-        my_stock.generate_stock_price_changes()
-        y = my_stock.value
-        line.set_data((x, y))
-        return line
-    
-    anim = FuncAnimation(fig, animate, frames=100, interval=20)
-    plt.show()
